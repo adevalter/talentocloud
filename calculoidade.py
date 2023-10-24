b1f435fe60101e384ea3ec064742fc7f4707ca2d
PontoCom <129428909+adevalter@users.noreply.github.com>
2023-10-24 07:58:33 -0300

    Corrigido ano e validado informação digitada no ano

--- calculoidade.py
+++ calculoidade.py
@@ -1,14 +1,17 @@
 def calculo(ano):
-    return 2023 - ano
+    return 2022 - ano
 executa = True
 while(executa):
     try:
-        print("Digite Seu Nome Completo :")
+        print("Digite Seu Nome:")
         nome = input()
-        print("Digite Ano Nascimento de 1992 a 2022 :")
+        print("digite Ano Nascicomento de 1922 a 2022:")
         ano = int(input())
-        resultado = calculo(ano)
-        print(f"Olá {nome} sua idade em 2023 completou ou completará {resultado}")
-        executa = False
+        if (ano >= 1922 and ano <= 2022):
+            resultado = calculo(ano)
+            print(f"Olá {nome} sua idade em 2022 é {resultado}")
+            executa = False
+        else:
+           print("Dados Invalidos Digite Ano Nascimento entre 1002 a 2022") 
     except:
         print("Dados incorretos")
