1262c90364f0fc3f61781c6e4ee5793f6e888071
Adevalter <adevalter@hotmail.com>
2024-01-06 22:50:27 -0300

    exercicio interpolação de strings

---
+++ js/modulo05/templatestring.js
@@ -0,0 +1,10 @@
+
+calcular(4, 5);
+
+function calcular(num1, num2,) {
+    console.log(`Soma de ${num1} + ${num2} é igual : ${num1 + num2}`);
+    console.log(`Substração de ${num1} - ${num2} é igual : ${num1 + num2}`);
+    console.log(`Multiplicação ${num1} X ${num2} é igual : ${num1 + num2}`);
+    console.log(`Divisão de ${num1} / ${num2} é igual : ${num1 + num2}`);
+
+}
