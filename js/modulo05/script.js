db6b985e1762e2817d07e805c2416f22ed475e8a
Adevalter <adevalter@hotmail.com>
2024-01-06 22:38:41 -0300

    exercicio innerText e innerHtml

---
+++ js/modulo05/script.js
@@ -0,0 +1,10 @@
+let hTitulo = document.querySelector("h1");
+let link = document.querySelector("a");
+let lista = document.querySelector("ol");
+hTitulo.innerText = "Talent Cloud"
+link.innerText = "Conheça a Escola"
+lista.innerHTML = `
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JAVASCRIPT</li>
+`;
